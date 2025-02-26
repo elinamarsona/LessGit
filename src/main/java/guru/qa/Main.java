6cc331148f447f2e34e342c7bf4701e04b126e5f
Elina Kulikova <elinamarsona@gmail.com>
2025-02-27 00:20:42 +0300

    commit 3

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,8 +3,8 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!");
-
-        System.out.println("Hello qa.guru!");
+        System.out.println("Hello qa!");
+        System.out.println("Hello guru!");
 
     }
 }
