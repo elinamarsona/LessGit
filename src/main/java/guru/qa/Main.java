3ccc8ced94486431ce50da493cff35e3ce920cec
elinamarsona <elinamarsona@gmail.com>
2025-02-27 00:27:40 +0300

    Update Main.java
    commit 3

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,8 +3,7 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!");
+        System.out.println("Hello guru!!!");
         System.out.println("Hello qa!");
-        System.out.println("Hello guru!");
-
-    }
+            }
 }
